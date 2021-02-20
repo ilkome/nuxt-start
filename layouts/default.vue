461ee743b66c56a3d1ac516b58dc540827c5f622
ilkome <ilya.komichev@gmail.com>
2021-02-20 08:53:59 +0300

    init base config

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
+
+<template lang="pug">
+Nuxt
+</template>
+
+<style lang="stylus" scoped>
+//
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<template lang="pug">
+div index
+</template>
+
+<style lang="stylus" scoped>
+//
+</style>
