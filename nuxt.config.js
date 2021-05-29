60a237f1280aa778bb772b47b9c554e71dc579e4
ilkome <ilya.komichev@gmail.com>
2021-05-29 09:49:31 +0300

    export composition-api/module'

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,9 +36,9 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://composition-api.nuxtjs.org
-    '@nuxtjs/composition-api',
+    '@nuxtjs/composition-api/module',
     // https://vite.nuxtjs.org/
-    // 'nuxt-vite'
+    'nuxt-vite'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
