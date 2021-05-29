7b027de849b8a2fef23e9f7cff6970d2bed40052
ilkome <ilya.komichev@gmail.com>
2021-05-29 14:53:47 +0300

    install vue-advanced-cropper

---
+++ components/Cropper.vue
@@ -0,0 +1,57 @@
+<script>
+import { Cropper, CircleStencil } from 'vue-advanced-cropper'
+import 'vue-advanced-cropper/dist/style.css'
+
+export default {
+  components: {
+    Cropper,
+    CircleStencil
+  },
+  data () {
+    return {
+      img: 'https://images.unsplash.com/photo-1600984575359-310ae7b6bdf2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+      stencilProps: {
+        handlers: {},
+        movable: false,
+        resizable: false,
+        aspectRatio: 1
+      },
+      resizeImage: {
+        adjustStencil: false
+      },
+      stencilSize: {
+        width: 600,
+        height: 600
+      },
+      imageRestriction: 'stencil'
+    }
+  },
+  methods: {
+    onChange ({ coordinates, canvas }) {
+      console.log(coordinates, canvas)
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+  Cropper(
+    class="cropper"
+    :auto-zoom="true"
+    :image-restriction="imageRestriction"
+    :resize-image="resizeImage"
+    :src="img"
+    :stencil-component="$options.components.CircleStencil"
+    :stencil-props="stencilProps"
+    :stencil-size="stencilSize"
+    @change="onChange"
+  )
+</template>
+
+<style scoped>
+.cropper {
+  width: 600px;
+	height: 600px;
+	background: #DDD;
+}
+</style>
---
+++ pages/cropper.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'CropperPage'
+}
+</script>
+
+<template lang="pug">
+Cropper
+</template>
+
+<style lang="stylus" scoped>
+//
+</style>
